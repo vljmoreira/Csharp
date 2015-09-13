cadc3513d633478b97a0e7555a113dfd831c4ce4
vljmoreira <vljmoreira@gmail.com>
2015-09-13 20:56:12 +0100

    Loops constructos ans encapsulation and abstraction concepts

---
+++ BasicPrincipals/BasicPrincipals/AcessSpecifiers/ProtectedAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicPrincipals.AcessSpecifiers
+{
+    class ProtectedAccess
+    {
+        protected string name;
+
+        public void print()
+        {
+            Console.WriteLine("\n My name is " + name);
+        }
+    }
+
+    class Program : ProtectedAccess //Herda da classe ProtectedAccess
+    {
+        static void Main(String[] args)
+        {
+            Program pAccess = new Program();
+
+            Console.WriteLine("Write your name:");
+            //Severity	Code	Description	Project	File	Line
+            //Error CS0122  'ProtectedAccess.name' is inaccessible due to its protection level BasicPrincipals 
+            pAccess.name = Console.ReadLine();
+            pAccess.print();
+
+            Console.ReadLine();
+        }
+    }
+}
